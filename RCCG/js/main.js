c780c463a1a928e105197550f0380f02dc8c01bc
Sam <s.salisu@live.co.uk>
2015-01-17 22:51:55 +0000

    WI

---
+++ RCCG/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by simba on 17/01/15.
+ */
+$(document).ready(function () {
+
+    $("#header").load("/templates/header.html");
+
+    $("#footer").load("/templates/footer.html");
+
+});
